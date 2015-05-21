e2cb8f3dde9c51a6748bc294f35a5faa69071261
AJ ONeal <awesome@coolaj86.com>
2015-05-21 00:11:43 -0600

    browser / node demo works

--- browser.js
+++ browser.js
@@ -1,54 +1,35 @@
 'use strict';
-console.log("WORK IN PROGRESS");
-console.log("");
-console.log("");
 
 var webcrypto = window.crypto || window.msCrypto || window.webkitCrypto || window.mozCrypto;
+
 var keyStr = 'VzC2coGPrvecrigzB38DRLGiwVrgiwnQznyrD9BYxAk=';
-var keyBuf = window.Unibabel.base64ToArr(keyStr); //.buffer;
-var message = "this is a secret message";
-// var iv = window.crypto.getRandomValues(new Uint8Array(16))
+var keyBuf = window.Unibabel.base64ToArr(keyStr);
+var message = "this is a secret message !¶☢☃𩶘𝄢";
 var ivLen = (128 / 8);
-var iv = new Uint8Array(ivLen);
+var iv = new Uint8Array(ivLen); // defaults to zero
 var arrb = window.Unibabel.strToUtf8Arr(message);
 var data = arrb.buffer;
-console.log('message as buffer');
+console.log('message as buffer', arrb.length);
 console.log(arrb);
-console.log(arrb.length);
-console.log('');
-/*
-var i;
-for (i = 0; i < ivLen; i += 1) {
-  iv[i] = 0;
-}
-*/
 
-console.log('keyBuf');
+console.log('keyBuf', keyBuf.length);
 console.log(keyBuf);
-console.log(keyBuf.length);
-console.log('');
 
 webcrypto.subtle.importKey(
-    "raw", //can be "jwk" or "raw"
-    keyBuf, // ArrayBuffer
-    {   //this is the algorithm options
-        name: "AES-CBC",
-    },
-    true, //whether the key is extractable (i.e. can be used in exportKey)
-    ["encrypt", "decrypt"] //can be any combination of "encrypt" and "decrypt"
+    "raw",
+    keyBuf,
+    { name: "AES-CBC" },
+    true,
+    ["encrypt", "decrypt"]
 )
 .then(function(key){
     //returns the symmetric key
     console.log('key', key);
-    console.log('');
+    var algo = key.algorithm;
+    algo.iv = iv;
 
     return window.crypto.subtle.encrypt(
-        {
-            name: "AES-CBC",
-            //Don't re-use initialization vectors!
-            //Always generate a new iv every time your encrypt!
-            iv: iv,
-        },
+        algo,
         key, //from generateKey or importKey above
         data //ArrayBuffer of data you want to encrypt
     ).then(function(encrypted){
@@ -62,21 +43,23 @@ webcrypto.subtle.importKey(
       console.log('encrypted', window.Unibabel.arrToBase64(new Uint8Array(encrypted)));
 
       window.crypto.subtle.decrypt(
-          {
-              name: "AES-CBC",
-              //Don't re-use initialization vectors!
-              //Always generate a new iv every time your encrypt!
-              iv: iv,
-          },
+          algo,
           key, //from generateKey or importKey above
-          data //ArrayBuffer of data you want to encrypt
+          encrypted //ArrayBuffer of data you want to encrypt
       )
       .then(function(decrypted){
           //returns an ArrayBuffer containing the encrypted data
-          // TODO base64
-          //console.log(new Uint8Array(encrypted));
-          console.log('decrypted', window.Unibabel.arrToBase64(new Uint8Array(decrypted)));
+          console.log(new Uint8Array(encrypted));
+          console.log('decrypted', window.Unibabel.utf8ArrToStr(new Uint8Array(decrypted)));
       })
+
+
+
+
+
+
+       // error handling
+
       .catch(function(err){
           console.error(err);
       });
--- node-decrypt.js
+++ node-decrypt.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var crypto = require("crypto");
-var encrypted = "dPluq7hwg9nML6DAlAoANOHy15fQCoxCOjr3Aah6bnygTNtLJ8MhFKYjlTXtIK0SulCQqI4x7tQEedo3tFmeB5ugoESAm8Fd7aqxrNk9JoHqJbwyTIKriSAvyKdfqUKWelDYvr35xEKrdF1wvoj5m2sDS4ppGgtRfXtzz2ZjTZHzja/jzicharb8gVOrIgqr";
+//var encrypted = "dPluq7hwg9nML6DAlAoANOHy15fQCoxCOjr3Aah6bnygTNtLJ8MhFKYjlTXtIK0SulCQqI4x7tQEedo3tFmeB5ugoESAm8Fd7aqxrNk9JoHqJbwyTIKriSAvyKdfqUKWelDYvr35xEKrdF1wvoj5m2sDS4ppGgtRfXtzz2ZjTZHzja/jzicharb8gVOrIgqr";
+var encrypted = "dPluq7hwg9nML6DAlAoANB4niyGBB8EOWqwG9Nsc+if3n2RuRhmOmupvAZN9wk8o";
 var buffer = new Buffer(encrypted, 'base64');
 var keyBuf = new Buffer('VzC2coGPrvecrigzB38DRLGiwVrgiwnQznyrD9BYxAk=', 'base64');
 // key = crypto.randomBytes((256 / 8)).toString('base64')

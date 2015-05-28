381835f4769abdace903b75b492ac12182d61402
AJ ONeal <awesome@coolaj86.com>
2015-05-27 23:27:33 -0600

    add pbkdf2 and examples

--- browser.js
+++ browser.js
@@ -1,73 +1,206 @@
+(function () {
 'use strict';
 
 var webcrypto = window.crypto || window.msCrypto || window.webkitCrypto || window.mozCrypto;
+if (webcrypto.webkitSubtle) {
+  webcrypto.subtle = webcrypto.webkitSubtle;
+}
+var Unibabel = window.Unibabel;
 
-var keyStr = 'VzC2coGPrvecrigzB38DRLGiwVrgiwnQznyrD9BYxAk=';
-var keyBuf = window.Unibabel.base64ToArr(keyStr);
-var message = "this is a secret message !¶☢☃𩶘𝄢";
-var ivLen = (128 / 8);
-var iv = new Uint8Array(ivLen); // defaults to zero
-var arrb = window.Unibabel.strToUtf8Arr(message);
-var data = arrb.buffer;
-console.log('message as buffer', arrb.length);
-console.log(arrb);
+function ab2hex(ab) {
+  var dv = new DataView(ab);
+  var i;
+  var len;
+  var hex = '';
+  var c;
 
-console.log('keyBuf', keyBuf.length);
-console.log(keyBuf);
+  for (i = 0, len = dv.byteLength; i < len; i += 1) {
+    c = dv.getUint8(i).toString(16);
+    if (c.length < 2) {
+      c = '0' + c;
+    }
+    hex += c;
+  }
 
-webcrypto.subtle.importKey(
-    "raw",
-    keyBuf,
-    { name: "AES-CBC" },
-    true,
-    ["encrypt", "decrypt"]
-)
-.then(function(key){
-    //returns the symmetric key
-    console.log('key', key);
-    var algo = key.algorithm;
-    algo.iv = iv;
+  return hex;
+}
+function hex2ab(hex) {
+  var i;
+  var byteLen = hex.length / 2;
+  var ab;
 
-    return window.crypto.subtle.encrypt(
-        algo,
-        key, //from generateKey or importKey above
-        data //ArrayBuffer of data you want to encrypt
-    ).then(function(encrypted){
-      //returns an ArrayBuffer containing the encrypted data
-      // TODO base64
-      //console.log(new Uint8Array(encrypted));
-      console.log('encrypted');
-      console.log(new Uint8Array(encrypted));
-      console.log(new Uint8Array(encrypted).length);
+  if (byteLen !== Math.ceil(byteLen)) {
+    console.log('ignoring bad hex length');
+    return;
+  }
 
-      console.log('encrypted', window.Unibabel.arrToBase64(new Uint8Array(encrypted)));
+  ab = new Uint8Array(byteLen);
 
-      window.crypto.subtle.decrypt(
-          algo,
-          key, //from generateKey or importKey above
-          encrypted //ArrayBuffer of data you want to encrypt
-      )
-      .then(function(decrypted){
+  for (i = 0; i < byteLen; i += 1) {
+    ab[i] = parseInt(hex[i] + hex[i + 1], 16);
+  }
+
+  return ab.buffer;
+}
+var arrayBufferToHexString = ab2hex;
+//var hexStringToArrayBuffer = hex2ab;
+var stringToArrayBuffer = Unibabel.strToUtf8Arr;
+
+  function encrypt(keyBuf, data) {
+    //var keyStr = 'VzC2coGPrvecrigzB38DRLGiwVrgiwnQznyrD9BYxAk=';
+    //var keyBuf = window.Unibabel.base64ToArr(keyStr);
+    //var message = "this is a secret message !¶☢☃𩶘𝄢";
+    var ivLen = (128 / 8);
+    var iv = new Uint8Array(ivLen); // defaults to all 0s, which is fine for this demo
+    // TODO derive iv from key?
+    //var iv = new Uint8Array(keyBuf.slice(0, ivLen));
+    //var arrb = window.Unibabel.strToUtf8Arr(message);
+    //var data = arrb.buffer;
+    //console.log('message as buffer', arrb.length);
+    //console.log(arrb);
+
+    console.log('iv', iv);
+    console.log('keyBuf', keyBuf.length);
+    console.log(keyBuf);
+
+    return webcrypto.subtle.importKey(
+        "raw",
+        keyBuf,
+        { name: "AES-CBC" },
+        true,
+        ["encrypt", "decrypt"]
+    )
+    .then(function(key){
+        //returns the symmetric key
+        console.log('key', key);
+        var algo = key.algorithm;
+        algo.iv = iv;
+
+        return webcrypto.subtle.encrypt(
+            algo,
+            key, //from generateKey or importKey above
+            data //ArrayBuffer of data you want to encrypt
+        ).then(function(encrypted){
           //returns an ArrayBuffer containing the encrypted data
+          // TODO base64
+          //console.log(new Uint8Array(encrypted));
+          console.log('encrypted');
           console.log(new Uint8Array(encrypted));
-          console.log('decrypted', window.Unibabel.utf8ArrToStr(new Uint8Array(decrypted)));
-      })
+          console.log(new Uint8Array(encrypted).length);
+
+          console.log('encrypted', window.Unibabel.arrToBase64(new Uint8Array(encrypted)));
+
+          return webcrypto.subtle.decrypt(
+              algo,
+              key, //from generateKey or importKey above
+              encrypted //ArrayBuffer of data you want to encrypt
+          )
+          .then(function(decrypted){
+              //returns an ArrayBuffer containing the encrypted data
+              console.log(new Uint8Array(encrypted));
+              console.log('decrypted', window.Unibabel.utf8ArrToStr(new Uint8Array(decrypted)));
+
+              return encrypted;
+          })
 
 
 
 
 
 
-       // error handling
+           // error handling
 
-      .catch(function(err){
-          console.error(err);
-      });
+          .catch(function(err){
+              console.error(err);
+          });
+        })
+        .catch(function(err){
+            console.error(err);
+        });
     })
     .catch(function(err){
         console.error(err);
     });
-})
-.catch(function(err){
-    console.error(err);
+  }
+
+$(function () {
+  var keyLen = 256;
+  // 100 - probably safe even on a browser running from a raspberry pi using pure js ployfill
+  // 10000 - no noticeable speed decrease on my MBP
+  // 100000 - you can notice
+  // 1000000 - annoyingly long
+  var iterations = 100; // something a browser on a raspberry pi or old phone could do
+  var hashname = "SHA-256";
+  // generated with Unibabel.arrToBase64(webcrypto.getRandomValues(new Uint8Array(32)));
+  var salt = Unibabel.base64ToArr("6FxT5/EZ1B/XiVzcnXu53RneW6vSwf30uNrXyzNE2IY=").buffer;
+  var kdfname = "PBKDF2";
+  var aesname = "AES-CBC"; // AES.CTR is also popular
+
+  function deriveKey(salt, passphrase, keyLen) {
+    // First, create a PBKDF2 "key" containing the password
+    return webcrypto.subtle.importKey(
+        "raw",
+        stringToArrayBuffer(passphrase),
+        {"name": kdfname},
+        false,
+        ["deriveKey"]).
+    // Derive a key from the password
+    then(function(baseKey){
+        return webcrypto.subtle.deriveKey(
+            {
+                "name": kdfname,
+                "salt": stringToArrayBuffer(salt),
+                "iterations": iterations,
+                "hash": hashname
+            },
+            baseKey,
+            { "name": aesname, "length": keyLen }, // Key we want
+            true,                                    // Extrable
+            [ "encrypt", "decrypt" ]                 // For new key
+            );
+    }).
+    // Export it so we can display it
+    then(function(aesKey) {
+        return webcrypto.subtle.exportKey("raw", aesKey);
+    }).
+    // Display it in hex format
+    then(function(keyBytes) {
+        var hexKey = arrayBufferToHexString(keyBytes);
+        return hexKey;
+    }).
+    catch(function(err) {
+        window.alert("Key derivation failed: " + err.message);
+    });
+  }
+
+  function docDeriveKey() {
+    var passphrase = $('.js-crypto-passphrase').val();
+    var salt = hex2ab($('.js-crypto-salt').val());
+    var msg = Unibabel.strToUtf8Arr($('.js-crypto-msg').val()).buffer;
+
+    console.log(msg);
+    webcrypto.subtle.digest("SHA-1", msg).then(function (sha1buf) {
+      $('.js-crypto-sha1').val(ab2hex(sha1buf));
+    });
+
+    return deriveKey(salt, passphrase, keyLen).then(function (hexkey) {
+      $('.js-crypto-key').val(hexkey);
+      return encrypt(hex2ab(hexkey), msg).then(function (buf) {
+        console.log('ciphered buf', buf);
+        $('.js-crypto-ciphered').val(ab2hex(buf));
+        //$('.js-crypto-ciphered').val(Unibabel.arrToBase64(new Uint8Array(buf)));
+      });
+    });
+  }
+
+
+  $('.js-crypto-salt').val(ab2hex(salt));
+
+  $('body').on('change', '.js-crypto-msg', docDeriveKey);
+  $('body').on('keyup', '.js-crypto-msg', docDeriveKey);
+  $('body').on('change', '.js-crypto-passphrase', docDeriveKey);
+  $('body').on('keyup', '.js-crypto-passphrase', docDeriveKey);
+  docDeriveKey();
 });
+
+}());
--- node-decrypt.js
+++ node-decrypt.js
@@ -2,11 +2,16 @@
 
 var crypto = require("crypto");
 //var encrypted = "dPluq7hwg9nML6DAlAoANOHy15fQCoxCOjr3Aah6bnygTNtLJ8MhFKYjlTXtIK0SulCQqI4x7tQEedo3tFmeB5ugoESAm8Fd7aqxrNk9JoHqJbwyTIKriSAvyKdfqUKWelDYvr35xEKrdF1wvoj5m2sDS4ppGgtRfXtzz2ZjTZHzja/jzicharb8gVOrIgqr";
-var encrypted = "dPluq7hwg9nML6DAlAoANB4niyGBB8EOWqwG9Nsc+if3n2RuRhmOmupvAZN9wk8o";
-var buffer = new Buffer(encrypted, 'base64');
-var keyBuf = new Buffer('VzC2coGPrvecrigzB38DRLGiwVrgiwnQznyrD9BYxAk=', 'base64');
+//var encrypted = "dPluq7hwg9nML6DAlAoANB4niyGBB8EOWqwG9Nsc+if3n2RuRhmOmupvAZN9wk8o";
+//var encrypted = "WZZZ2zQiDBapiAkmA+CCMJM6x8K66bToJ9PsBW/hxBoy9OnTZ3Q78O15NK0vbgkOHoM4Mo63I8CbGHtHzaDG4+bnOS9KyFjt2K3VcVRqLlNCr6Nba24snDLZHvWTeRQu";
+//var buffer = new Buffer(encrypted, 'base64');
+var encrypted = "599659db34220c16a988092603e08230933ac7c2bae9b4e827d3ec056fe1c41a32f4e9d367743bf0ed7934ad2f6e090e1e8338328eb723c09b187b47cda0c6e3e6e7392f4ac858edd8add571546a2e5342afa35b6b6e2c9c32d91ef59379142e";
+var buffer = new Buffer(encrypted, 'hex');
+//var keyBuf = new Buffer('VzC2coGPrvecrigzB38DRLGiwVrgiwnQznyrD9BYxAk=', 'base64');
+var keyBuf = new Buffer('a29944a586a03d56ea963bdfca3879aac4a5a44da935fe883ec7909aeca9b040', 'hex');
 // key = crypto.randomBytes((256 / 8)).toString('base64')
 // required 256 bits (same as keysize)
+console.log(keyBuf);
 
 function createIv() {
   // IV is always (?) 128-bit
@@ -20,9 +25,19 @@ function createIv() {
   return iv;
 }
 
-var iv = createIv();
+/*
+function createIv(buf) {
+  // IV is always (?) 128-bit
+  var ivLen = (128 / 8);
+  var iv = buf.slice(0, ivLen);
+  return iv;
+}
+*/
+
+var iv = createIv(keyBuf);
 var decipher = crypto.createDecipheriv('aes-256-cbc', keyBuf, iv);
 var message = decipher.update(buffer, null, 'utf8');
+console.log(message);
 message += decipher.final('utf8');
 
 console.log(message); // "this is a secret message"
